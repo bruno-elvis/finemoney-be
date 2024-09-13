3cf827ac858a5a377f614022caa771859faf25b8
Bruno-Elvis <brunoelvis@outlook.com.br>
2024-09-13 14:11:58 -0300

    Criação de custom decorators para definição das rotas públicas da aplicação e obtenção do id do usuário ativo

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,14 +1,13 @@
-import { Controller, Get, Request } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
 
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
 	@Get('/me')
-	me(@Request() request: any) {
-		console.log({ meUserId: request.userId });
-
-		return this.usersService.getUserById('userId');
+	me(@ActiveUserId() userId: string) {
+		return this.usersService.getUserById(userId);
 	}
 }
---
+++ src/shared/decorators/ActiveUserId.ts
@@ -0,0 +1,16 @@
+import {
+	createParamDecorator,
+	ExecutionContext,
+	UnauthorizedException,
+} from '@nestjs/common';
+
+export const ActiveUserId = createParamDecorator<undefined>(
+	(data, context: ExecutionContext) => {
+		const request = context.switchToHttp().getRequest();
+		const { userId } = request;
+
+		if (!userId) throw new UnauthorizedException();
+
+		return userId;
+	},
+);
